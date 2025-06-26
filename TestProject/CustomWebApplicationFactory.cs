1876372f034d5e32f85bfd65b8cc2dd16b1c8ba2
Bas Visscher <bas@koolecontrols.nl>
2025-06-26 10:39:11 +0200

    Add project and initial test

---
+++ TestProject/CustomWebApplicationFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using api_test_project;
+
+namespace TestProject
+{
+
+
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development");
+            // Add test-specific service overrides here if needed
+        }
+    }
+
+
+}
---
+++ TestProject/WeatherForecastTests.cs
@@ -0,0 +1,29 @@
+﻿using api_test_project;
+using System.Net.Http.Json;
+
+namespace TestProject
+{
+    public class WeatherForecastTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public WeatherForecastTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsWeatherForecasts()
+        {
+
+            // Act
+            var response = await _client.GetAsync("/WeatherForecast");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var forecasts = await response.Content.ReadFromJsonAsync<WeatherForecast[]>();
+
+        }
+    }
+}
