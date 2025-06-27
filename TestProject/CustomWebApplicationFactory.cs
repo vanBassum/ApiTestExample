71ca4f8ecdbc1afb24ec32ef12191076ee8843a0
Bas Visscher <bas@koolecontrols.nl>
2025-06-27 14:02:47 +0200

    Try to automatically generate migrations

--- TestProject/CustomWebApplicationFactory.cs
+++ TestProject/CustomWebApplicationFactory.cs
@@ -12,33 +12,9 @@ namespace TestProject
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseEnvironment("Development");
-
             builder.ConfigureServices(services =>
             {
-                // Remove all existing registrations related to ApplicationDbContext
-                var descriptorsToRemove = services
-                    .Where(d => d.ServiceType.FullName != null &&
-                                d.ServiceType.FullName.Contains(nameof(ApplicationDbContext)))
-                    .ToList();
-
-                foreach (var descriptor in descriptorsToRemove)
-                {
-                    services.Remove(descriptor);
-                }
-
-                // Register new in-memory version
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseInMemoryDatabase("TestDb"));
-
-                // Build and ensure DB is created
-                var sp = services.BuildServiceProvider();
-                using var scope = sp.CreateScope();
-                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                db.Database.EnsureCreated();
-
-                // Optional: seed data
-                // db.YourTable.Add(...); db.SaveChanges();
+                
             });
         }
     }
